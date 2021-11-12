e673305caefbfd78fa06f6cd34e918c7bd83df1d
basilelkhalifa <basilosama.2000@gmail.com>
2021-11-12 21:51:15 +0300

    initialization

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import React from "react";
+import { Box, Typography } from "@mui/material";
 
 export default function App() {
-  return <div></div>;
+  return (
+    <Box>
+      <Typography
+        sx={{
+          position: "absolute",
+        }}
+        variant="h1"
+      >
+        UNDER CONSTRUCTION
+      </Typography>
+    </Box>
+  );
 }
