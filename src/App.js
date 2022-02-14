725228a93f16cba6b840a4e3ab874b42045220eb
basilelkhalifa <basilosama.2000@gmail.com>
2022-02-15 00:56:03 +0300

    changed default font

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import React from "react";
 import { Box } from "@mui/material";
 import DesktopContainer from "./components/Desktop/DesktopContainer";
 import "./index.css";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: `"Tahoma", "Helvetica", "Arial", sans-serif`,
+    fontSize: 14,
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightMedium: 500,
+  },
+});
 
 export default function App() {
   return (
-    <Box>
-      <DesktopContainer />
-    </Box>
+    <ThemeProvider theme={theme}>
+      <Box>
+        <DesktopContainer />
+      </Box>
+    </ThemeProvider>
   );
 }
