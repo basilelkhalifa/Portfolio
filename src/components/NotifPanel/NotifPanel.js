9857e6e6a532d68f9da2facb2de5155e467e3181
basilelkhalifa <basilosama.2000@gmail.com>
2022-02-15 15:29:37 +0300

    added icons to panel

--- src/components/NotifPanel/NotifPanel.js
+++ src/components/NotifPanel/NotifPanel.js
@@ -1,15 +1,22 @@
 import React from "react";
 import { Box, Typography } from "@mui/material";
 import useStyles from "./style";
+import soundIcon from "../../assets/icons/audio-volume-muted.png";
+import battery from "../../assets/icons/notification-power.png";
 
 export default function NotifPanel() {
   const classes = useStyles();
   return (
     <Box className={classes.mainContainer}>
-      <Box className={classes.iconsContainer}></Box>
-      <Typography variant="caption" color="white">
-        11:00 AM
-      </Typography>
+      <Box className={classes.iconsContainer}>
+        <img src={soundIcon} className={classes.icon} alt="Sound Icon" />
+        <img src={battery} className={classes.icon} alt="Battery Icon" />
+      </Box>
+      <Box className={classes.timeContainer}>
+        <Typography variant="caption" color="white">
+          11:00 AM
+        </Typography>
+      </Box>
     </Box>
   );
 }
--- src/components/NotifPanel/style.js
+++ src/components/NotifPanel/style.js
@@ -5,6 +5,23 @@ const useStyles = makeStyles({
     background: "linear-gradient(180deg, #46B1FF 0%, #0071C4 100%)",
     borderRadius: "1px",
     height: "2rem",
+    display: "flex",
+    alignItems: "center",
+  },
+
+  icon: {
+    height: "24px",
+    width: "24px",
+  },
+
+  iconsContainer: {
+    display: "flex",
+    alignItems: "center",
+    padding: "0 0 0 0.5rem",
+  },
+
+  timeContainer: {
+    padding: "0 0.5rem",
   },
 });
 
