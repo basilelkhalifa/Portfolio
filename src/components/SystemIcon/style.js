5b1fb2ca13d983a187d05d0e1859c7e2e979b429
basilelkhalifa <basilosama.2000@gmail.com>
2022-02-15 22:36:13 +0300

    added text shadow

--- src/components/SystemIcon/style.js
+++ src/components/SystemIcon/style.js
@@ -8,9 +8,14 @@ const useStyles = makeStyles({
     alignItems: "center",
     width: "42px",
   },
+
   icon: {
     width: "32px",
     height: "32px",
+  },
+
+  title: {
+    textShadow: "0px 4px 4px rgba(0, 0, 0, 0.5)",
   },
 });
 
